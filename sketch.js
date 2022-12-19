99b73fc4757e367a7d99e69b653eca2ee6c9b53e
Inventor4u <78658626+Inventor4u@users.noreply.github.com>
2022-12-19 16:24:20 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -73,21 +73,17 @@ function draw() {
 //   balloon.y = height.y;
 // }
 
-// function readHeight(data){
-//   height = data.val();
-//   balloon.x = height.x;
-//   balloon.y = height.y;
-// }
-
-// function readHeight(data){
-//   height = data.val();
-// }
-
- function readHeight(){
-   height = val();
+ function readHeight(data){
+   height = data.val();
    balloon.x = height.x;
    balloon.y = height.y;
 }
+
+// function readHeight(data){
+//   height = data.val();
+// }
+
+
 
 function showError(){
   console.log("Error in writing to the database");
